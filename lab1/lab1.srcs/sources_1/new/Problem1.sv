9674496200cb44c0f8e4d2df2e35c74a70e1f5e0
Ben Paulson <30847493+ben-paulson@users.noreply.github.com>
2020-07-06 09:12:28 -1000

    Add source for problem 1

---
+++ lab1/lab1.srcs/sources_1/new/Problem1.sv
@@ -0,0 +1,13 @@
+`timescale 1ns / 1ps
+
+module Problem1(
+    input A,
+    input B,
+    input C,
+    input D,
+    output F
+    );
+    
+    assign F = (~A & B & ~C) | (A & ~B & C) | (~A & D) | (~A & ~B & C) | (A & C & ~D);
+    
+endmodule
