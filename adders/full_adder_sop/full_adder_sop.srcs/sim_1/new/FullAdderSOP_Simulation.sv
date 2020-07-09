f4b11b26a0f4d583b26e9ed81b30f1f7f4126c1b
Ben Paulson <30847493+ben-paulson@users.noreply.github.com>
2020-07-09 13:34:50 -1000

    Simulate full adder in SOP form

---
+++ adders/full_adder_sop/full_adder_sop.srcs/sim_1/new/FullAdderSOP_Simulation.sv
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+
+module FullAdderSOP_Simulation();
+
+    logic sOP_A, sOP_B, sCin, sSUM, sCO;
+    
+    FullAdderSOP UUT (
+        .OP_A(sOP_A), .OP_B(sOP_B),
+        .Cin(sCin), .SUM(sSUM), .CO(sCO));
+        
+    initial begin
+    
+        for (byte a = 0; a < 2; a++) begin
+            for (byte b = 0; b < 2; b++) begin
+                for (byte cin = 0; cin < 2; cin++) begin
+                    sOP_A = a; sOP_B = b;
+                    sCin = cin;
+                    #10;
+                end
+            end
+        end
+    
+    end
+
+endmodule
