1479b48655276a198ba926a2e529bbdf6fec01a0
Ben Paulson <30847493+ben-paulson@users.noreply.github.com>
2020-07-09 15:00:13 -1000

    Create source for full adder in POS form

---
+++ adders/full_adder_pos/full_adder_pos.srcs/sources_1/new/FullAdderPOS.sv
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+
+module FullAdderPOS(
+    input Cin,
+    input OP_A,
+    input OP_B,
+    output CO_SOP,
+    output SUM_SOP,
+    output CO_POS,
+    output SUM_POS
+    );
+    
+    // SOP Equations
+    assign SUM_SOP = (~OP_A & ~OP_B & Cin) | (~OP_A & OP_B & ~Cin) |
+                 (OP_A & ~OP_B & ~Cin) | (OP_A & OP_B & Cin);
+    assign CO_SOP = (~OP_A & OP_B & Cin) | (OP_A & ~OP_B & Cin) |
+                (OP_A & OP_B & ~Cin) | (OP_A & OP_B & Cin);
+                
+    // POS equations
+    assign SUM_POS = (OP_A | OP_B | Cin) & (OP_A | ~OP_B | ~Cin) &
+                     (~OP_A | OP_B | ~Cin) & (~OP_A | ~OP_B | Cin);
+    assign CO_POS = (OP_A | OP_B | Cin) & (OP_A | OP_B | ~Cin) &
+                    (OP_A | ~OP_B | ~Cin) & (~OP_A | OP_B | Cin);
+    
+endmodule
