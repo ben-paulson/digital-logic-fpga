e6b49a1fff8a17a27999b7c725d68dff55cbfac2
Ben Paulson <30847493+ben-paulson@users.noreply.github.com>
2020-07-11 18:00:36 -1000

    Remove auto-generated comments

--- adders/ripple_carry_adder_5bit/ripple_carry_adder_5bit.srcs/sources_1/new/RippleCarryAdder.sv
+++ adders/ripple_carry_adder_5bit/ripple_carry_adder_5bit.srcs/sources_1/new/RippleCarryAdder.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07/10/2020 08:09:02 AM
-// Design Name: 
-// Module Name: RippleCarryAdder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module RippleCarryAdder(
     input [4:0] A,
