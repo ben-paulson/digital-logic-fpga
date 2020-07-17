09cf684b696f96c6c87f1fc55aa00b4d77e0d20d
Ben Paulson <30847493+ben-paulson@users.noreply.github.com>
2020-07-16 15:25:36 -1000

    Add decimal point output to 7-seg display

---
+++ bcd_seven_segment/bcd_seven_segment.srcs/sim_1/new/BCDSevenSegmentDecoder_Simulation.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/16/2020 03:07:52 PM
+// Design Name: 
+// Module Name: BCDSevenSegmentDecoder_Simulation
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module BCDSevenSegmentDecoder_Simulation(
+
+    );
+endmodule
--- bcd_seven_segment/bcd_seven_segment.srcs/sources_1/new/BCDSevenSegmentDecoder.sv
+++ bcd_seven_segment/bcd_seven_segment.srcs/sources_1/new/BCDSevenSegmentDecoder.sv
@@ -9,30 +9,32 @@
 module BCDSevenSegmentDecoder(
     input [3:0] enable,
     input [3:0] bcd,
-    output [6:0] seg,
+    output [7:0] seg,
     output [3:0] an
     );
     
     // Anodes - which segment sections to use
     assign an = ~enable;
     // Segment A
-    assign seg[6] = (~bcd[3] & bcd[1]) | (bcd[3] & ~bcd[2] & ~bcd[1]) |
-                    (~bcd[2] & ~bcd[1] & ~bcd[0]) | (~bcd[3] & bcd[2] & bcd[0]);
+    assign seg[7] = ~((~bcd[3] & bcd[1]) | (bcd[3] & ~bcd[2] & ~bcd[1]) |
+                    (~bcd[2] & ~bcd[1] & ~bcd[0]) | (~bcd[3] & bcd[2] & bcd[0]));
     // Segment B
-    assign seg[5] = (~bcd[3] & ~bcd[2]) | (~bcd[2] & ~bcd[1]) |
-                    (~bcd[3] & ~(bcd[1] ^ bcd[0]));
+    assign seg[6] = ~((~bcd[3] & ~bcd[2]) | (~bcd[2] & ~bcd[1]) |
+                    (~bcd[3] & ~(bcd[1] ^ bcd[0])));
     // Segment C
-    assign seg[4] = (~bcd[2] & ~bcd[1]) | (~bcd[3] & bcd[2]) | (~bcd[3] & bcd[0]);
+    assign seg[5] = ~((~bcd[2] & ~bcd[1]) | (~bcd[3] & bcd[2]) | (~bcd[3] & bcd[0]));
     // Segment D
-    assign seg[3] = (~bcd[2] & ~bcd[1] & ~bcd[0]) | (~bcd[3] & ~bcd[2] & bcd[1]) |
-                    (~bcd[3] & bcd[1] & ~bcd[0]) | (~bcd[3] & bcd[2] & ~bcd[1] & bcd[0]);
+    assign seg[4] = ~((~bcd[2] & ~bcd[1] & ~bcd[0]) | (~bcd[3] & ~bcd[2] & bcd[1]) |
+                    (~bcd[3] & bcd[1] & ~bcd[0]) | (~bcd[3] & bcd[2] & ~bcd[1] & bcd[0]));
     // Segment E
-    assign seg[2] = (~bcd[2] & ~bcd[1] & ~bcd[0]) | (~bcd[3] & bcd[1] & ~bcd[0]);
+    assign seg[3] = ~((~bcd[2] & ~bcd[1] & ~bcd[0]) | (~bcd[3] & bcd[1] & ~bcd[0]));
     // Segment F
-    assign seg[1] = (~bcd[1] & (bcd[3] ^ bcd[2])) | (~bcd[3] & ~bcd[1] & ~bcd[0]) |
-                    (~bcd[3] & bcd[2] & ~bcd[0]);
+    assign seg[2] = ~((~bcd[1] & (bcd[3] ^ bcd[2])) | (~bcd[3] & ~bcd[1] & ~bcd[0]) |
+                    (~bcd[3] & bcd[2] & ~bcd[0]));
     // Segment G
-    assign seg[0] = (~bcd[1] & (bcd[3] ^ bcd[2])) | (~bcd[3] & ~bcd[2] & bcd[1]) |
-                    (~bcd[3] & bcd[1] & ~bcd[0]);
+    assign seg[1] = ~((~bcd[1] & (bcd[3] ^ bcd[2])) | (~bcd[3] & ~bcd[2] & bcd[1]) |
+                    (~bcd[3] & bcd[1] & ~bcd[0]));
+    // Decimal point
+    assign seg[0] = 0;
     
 endmodule
