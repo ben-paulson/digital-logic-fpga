2e7d1ac294154a04eed0dcfc24ae5e38d5dfae89
Ben Paulson <30847493+ben-paulson@users.noreply.github.com>
2020-07-18 13:54:29 -1000

    Finish simulation

--- bcd_seven_segment/bcd_seven_segment.srcs/sim_1/new/BCDSevenSegmentDecoder_Simulation.sv
+++ bcd_seven_segment/bcd_seven_segment.srcs/sim_1/new/BCDSevenSegmentDecoder_Simulation.sv
@@ -1,26 +1,26 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07/16/2020 03:07:52 PM
-// Design Name: 
-// Module Name: BCDSevenSegmentDecoder_Simulation
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
+module BCDSevenSegmentDecoder_Simulation();
 
-module BCDSevenSegmentDecoder_Simulation(
+    logic [3:0] sBcd, sEnable, sAn;
+    logic [7:0] sSeg;
+    
+    BCDSevenSegmentDecoder UUT(
+        .bcd(sBcd), .enable(sEnable),
+        .seg(sSeg), .an(sAn));
+        
+    initial begin
+        /*
+        * Loop through all possible values and combinations
+        * for bcd and enable inputs. Both are 4-bits so 0-15.
+        */
+        for (byte i = 0; i < 16; i++) begin
+            for (byte j = 0; j < 16; j++) begin
+                sBcd = i;
+                sEnable = j;
+                #10;
+            end
+        end
+    end
 
-    );
 endmodule
