551afbc8fe6cf52a2c4f52ab9dec48a049a818bb
Ben Paulson <30847493+ben-paulson@users.noreply.github.com>
2020-07-24 15:20:57 -1000

    Add digits A-F for full hex display on 7-segment module

--- calculator/calculator.srcs/sources_1/new/SevenSegmentDecoder.sv
+++ calculator/calculator.srcs/sources_1/new/SevenSegmentDecoder.sv
@@ -20,6 +20,12 @@ module SevenSegmentDecoder(
             7: seg <= 7'b0001111;
             8: seg <= 7'b0000000;
             9: seg <= 7'b0001100;
+            10: seg <= 7'b0001000;
+            11: seg <= 7'b1100000;
+            12: seg <= 7'b0110001;
+            13: seg <= 7'b1000010;
+            14: seg <= 7'b0110000;
+            15: seg <= 7'b0111000;
             default: seg <= 7'b1111111; // Default to turn it off
         endcase
     
